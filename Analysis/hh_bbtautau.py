9ceb69ffca5e65dd92e575e0473fe8ef7d1467e8
Towsifa Akhter <towsifa.akhter@gmail.com>
2026-05-04 22:46:30 +0200

    formatting

--- Analysis/hh_bbtautau.py
+++ Analysis/hh_bbtautau.py
@@ -309,8 +309,8 @@ class DataFrameBuilderForHistograms(DataFrameBuilderBase):
         # Run 3
         self.df = self.df.Define(
             "SelectedFatJet_particleNet_XbbVsQCD_boosted_vec",
-            "SelectedFatJet_particleNet_XbbVsQCD[fatJet_sel];",# FatJet_particleNet_XbbVsQCD: ParticleNet X->bb vs. QCD score: Xbb/(Xbb+QCD)
-        )
+            f"SelectedFatJet_particleNet_XbbVsQCD[fatJet_sel];",
+        ) # FatJet_particleNet_XbbVsQCD: ParticleNet X->bb vs. QCD score: Xbb/(Xbb+QCD)
         self.df = self.df.Define(
             "SelectedFatJet_idxUnordered",
             "CreateIndexes(SelectedFatJet_p4[fatJet_sel].size())",
