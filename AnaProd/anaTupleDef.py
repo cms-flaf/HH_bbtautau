fa5e9e8dcfc4dbaa664f8eb7b1ca0841de6777d8
Sayan Dhani <sayandhani1234@gmail.com>
2026-05-29 13:41:05 +0200

    Dy corr global hist tuple (#60)
    * v3 use PNet, so Jet_btagDeepFlavB -> Jet_btagPNetB
    
    * v3 use PNet, so Jet_btagDeepFlavB -> Jet_btagPNetB
    
    * v3 use PNet, so Jet_btagDeepFlavB -> Jet_btagPNetB
    
    * added tau_{1,2}_gen_vis_p4 in anaTupleDef
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * DY_correction in HistTuple stage
    
    * Update Corrections submodule to main
    
    ---------
    
    Co-authored-by: Sayan <sadhani@cern.ch>

--- AnaProd/anaTupleDef.py
+++ AnaProd/anaTupleDef.py
@@ -3,6 +3,7 @@ import AnaProd.baseline as AnaBaseline
 import FLAF.Common.BaselineSelection as CommonBaseline
 from Corrections.Corrections import Corrections
 import ROOT
+from FLAF.Common.Utilities import defineP4
 
 loadTF = True
 loadHHBtag = True
@@ -585,6 +586,7 @@ def addAllVariables(
                     var_cond=f"tau{leg_idx+1}_genMatchIdx>=0",
                     default="-1.f",
                 )
+            defineP4(dfw, f"tau{leg_idx+1}_gen_vis")
             LegVar(
                 "gen_nChHad",
                 f"genLeptons.at(tau{leg_idx+1}_genMatchIdx).nChargedHadrons()",
